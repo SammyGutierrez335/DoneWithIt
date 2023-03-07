eb048ce4e197f27c7ce4463b657ff6902cd7bbfa
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-06 19:51:51 -0600

    first change of view

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 //View is like a div in the web world
 //Text is used to render text on the screen
 
-//react-native will map View depending on OS
+//react-native will map View to native widgets depending on OS
 //View -> on iOS will become UIView and AndroidView on Android
+
+//xcode is a great iOS emulsator tool
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Done With It!</Text>
       <StatusBar style="auto" />
     </View>
   );
