294706ee862fa6fa668e34c32e703e105502fd2f
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-06 20:03:23 -0600

    passes numberOfLines prop and onPress function to Text component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 //View is like a div in the web world
 //Text is used to render text on the screen
 
@@ -7,20 +7,26 @@ import { StyleSheet, Text, View } from 'react-native';
 //View -> on iOS will become UIView and AndroidView on Android
 
 //xcode is a great iOS emulsator tool
+
+//safe area view provides some padding to try to account for phone bezzles/notches
 export default function App() {
+
+  const handlePress = () => console.log("This text has been pressed")
   return (
-    <View style={styles.container}>
-      <Text>Done With It!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Done With It! - An app for selling stuff that you haven't used in years!
+        </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    alignContent: "center",
+    justifyContent: "center"
   },
 });
