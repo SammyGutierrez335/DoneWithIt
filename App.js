ccc9f72b7f5ddfb82bd2f293092c6f9569973fb7
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-08 12:45:42 -0600

    moves previous code to 'Notes.js' file and creates a new 'App.js'. Begins by creating a view grabbing dimensions from Dimensions API

--- App.js
+++ App.js
@@ -1,102 +1,42 @@
   import { 
-  SafeAreaView , Button, StyleSheet, Alert, 
-  StatusBar, Platform
+  SafeAreaView, StyleSheet, View, StatusBar, Dimensions
 } from 'react-native';
 
+//default value is Density-independent Pixels ()
+//actually Physical Pixels = DIPs x Scale Factor of device
+//ex on Model iPhone 4, 4s:
+  //Points 320 x 480
+  //Scale Factor of 2 (2 pixels per point)
+  //Pixels on screen 640 x 960
+  //View Width for 150 with be DIPs (150) x 2 = 300
+
+//iPhone 11Pro Max
+  //Points 414 x 896
+  //scale factor of 3
+  //Pixels on screen 1242 x 2688
+  //150 x 3 = 450
+
 export default function App() {
+
+  //this code below will not respond to orientation changes
+  //'screen' returns the size of the entire screen while 'window' returns the visible application window (iOS dimensions are equal, different on android)
+  console.log(Dimensions.get('screen'))
   return (
-    // You can pass multiple styles using an array. The result is the combination of styles similar to how you use multiple css classes. The object on the right would over right what is on the left.
-    //style=[containerStyle, styles.container]
-    //it is conventional to apply styles below component but is you can also import styles from another file.
     <SafeAreaView style={styles.container}>
-      <Button 
-        color="orange"
-        title="Click Me" 
-        // onPress={() => 
-          // this api only works on iOS
-          // Alert.prompt("Fill in the Blank", "What will you type?", answer => console.log(answer))
-        // }
-        // The Alert is not a component with a visual representation but instead an api, object with various methods
-        //ctrl + spacebar while inside an object to see what propeties an object contains
-        // array of buttons is the third parameter 
-        onPress={() => Alert.alert("Question", "Will you press yes?", [
-          {text: "Yes", onPress: () => console.log("Yes was pressed")},
-          {text: "No", onPress: () => console.log("No was pressed")}
-        ])}
-        // onPress={() => alert("button pressed")}
-        // onPress={()=> console.log("button pressed")}
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        // width: 150,
+        width: '50%',
+        height: 70
+      }} 
       />
     </SafeAreaView>
   )
 }
-
-// The code below was used for familiarizing myself with view, image and touchable components.
-
-// import { StatusBar } from 'expo-status-bar';
-// import { 
-//   StyleSheet, Text, View, 
-//   SafeAreaView , Image, 
-//   // TouchableWithoutFeedback
-//   // TouchableOpacity,
-//   // TouchableHighlight,
-//   TouchableNativeFeedback
-// } from 'react-native';
-// //View is like a div in the web world
-// //Text is used to render text on the screen
-
-// //react-native will map View to native widgets depending on OS
-// //View -> on iOS will become UIView and AndroidView on Android
-
-// //xcode is a great iOS emulsator tool
-
-// //safe area view provides some padding to try to account for phone bezzles/notches
-// export default function App() {
-
-
-//   // when we use the require function, react native package will include this file in the bundle (increasing size of app)
-//   // you can also download files from the internet to minimize app size.
-//   // !! the require function will return a reference to the image not the actual image itself
-//   // console.log(('require("./assets/icon.png'));
-//   // const handlePress = () => console.log("Image Pressed")
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Text numberOfLines={1}>Done With It!</Text>
-//       {/* touchable native feedback isn't supported in iOS and throws a warning. It also has no effect on images. */}
-//       <TouchableNativeFeedback>
-//         <View style={{ width: 200, height: 70, backgroundColor: "green"}}></View>
-//       </TouchableNativeFeedback>
-//       {/* <TouchableWithoutFeedback onPress={handlePress}> */}
-//       {/* <TouchableOpacity> */}
-//       {/* <TouchableHighlight> */}
-//         {/* <Image 
-//           // blurRadius={10}
-//           fadeDuration={1000}
-//           // source={require('./assets/icon.png')}
-//           resizeMode={"contain"}
-//           source={{
-//             width: 200,
-//             height: 300,
-//             uri: "https://picsum.photos/200/300"
-//           }}
-//         /> */}
-//       {/* </TouchableHighlight> */}
-//       {/* </TouchableOpacity> */}
-//       {/* </TouchableWithoutFeedback> */}
-//       <StatusBar style="auto" />
-//     </SafeAreaView>
-//   )
-// }
-
-//this is not css, but a plan JS object. React-native will map over these properties to build styles
-// const containerStyle={backgroundColor: "orange"}
-
-//Whatever object you pass to the create method you get as a result. The purposes are
-//validates the properties you pass in (throwing errors for you if you misspell a property)
-//the react-native team is working on implementation some optimizations for this, therefore it is the recommended practice. 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : '0'
   },
 });
---
+++ Notes.js
@@ -0,0 +1,102 @@
+//   import { 
+//   SafeAreaView , Button, StyleSheet, Alert, 
+//   StatusBar, Platform
+// } from 'react-native';
+
+// export default function App() {
+//   return (
+//     // You can pass multiple styles using an array. The result is the combination of styles similar to how you use multiple css classes. The object on the right would over right what is on the left.
+//     //style=[containerStyle, styles.container]
+//     //it is conventional to apply styles below component but is you can also import styles from another file.
+//     <SafeAreaView style={styles.container}>
+//       <Button 
+//         color="orange"
+//         title="Click Me" 
+//         // onPress={() => 
+//           // this api only works on iOS
+//           // Alert.prompt("Fill in the Blank", "What will you type?", answer => console.log(answer))
+//         // }
+//         // The Alert is not a component with a visual representation but instead an api, object with various methods
+//         //ctrl + spacebar while inside an object to see what propeties an object contains
+//         // array of buttons is the third parameter 
+//         onPress={() => Alert.alert("Question", "Will you press yes?", [
+//           {text: "Yes", onPress: () => console.log("Yes was pressed")},
+//           {text: "No", onPress: () => console.log("No was pressed")}
+//         ])}
+//         // onPress={() => alert("button pressed")}
+//         // onPress={()=> console.log("button pressed")}
+//       />
+//     </SafeAreaView>
+//   )
+// }
+
+// // The code below was used for familiarizing myself with view, image and touchable components.
+
+// // import { StatusBar } from 'expo-status-bar';
+// // import { 
+// //   StyleSheet, Text, View, 
+// //   SafeAreaView , Image, 
+// //   // TouchableWithoutFeedback
+// //   // TouchableOpacity,
+// //   // TouchableHighlight,
+// //   TouchableNativeFeedback
+// // } from 'react-native';
+// // //View is like a div in the web world
+// // //Text is used to render text on the screen
+
+// // //react-native will map View to native widgets depending on OS
+// // //View -> on iOS will become UIView and AndroidView on Android
+
+// // //xcode is a great iOS emulsator tool
+
+// // //safe area view provides some padding to try to account for phone bezzles/notches
+// // export default function App() {
+
+
+// //   // when we use the require function, react native package will include this file in the bundle (increasing size of app)
+// //   // you can also download files from the internet to minimize app size.
+// //   // !! the require function will return a reference to the image not the actual image itself
+// //   // console.log(('require("./assets/icon.png'));
+// //   // const handlePress = () => console.log("Image Pressed")
+// //   return (
+// //     <SafeAreaView style={styles.container}>
+// //       <Text numberOfLines={1}>Done With It!</Text>
+// //       {/* touchable native feedback isn't supported in iOS and throws a warning. It also has no effect on images. */}
+// //       <TouchableNativeFeedback>
+// //         <View style={{ width: 200, height: 70, backgroundColor: "green"}}></View>
+// //       </TouchableNativeFeedback>
+// //       {/* <TouchableWithoutFeedback onPress={handlePress}> */}
+// //       {/* <TouchableOpacity> */}
+// //       {/* <TouchableHighlight> */}
+// //         {/* <Image 
+// //           // blurRadius={10}
+// //           fadeDuration={1000}
+// //           // source={require('./assets/icon.png')}
+// //           resizeMode={"contain"}
+// //           source={{
+// //             width: 200,
+// //             height: 300,
+// //             uri: "https://picsum.photos/200/300"
+// //           }}
+// //         /> */}
+// //       {/* </TouchableHighlight> */}
+// //       {/* </TouchableOpacity> */}
+// //       {/* </TouchableWithoutFeedback> */}
+// //       <StatusBar style="auto" />
+// //     </SafeAreaView>
+// //   )
+// // }
+
+// //this is not css, but a plan JS object. React-native will map over these properties to build styles
+// // const containerStyle={backgroundColor: "orange"}
+
+// //Whatever object you pass to the create method you get as a result. The purposes are
+// //validates the properties you pass in (throwing errors for you if you misspell a property)
+// //the react-native team is working on implementation some optimizations for this, therefore it is the recommended practice. 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'dodgerblue',
+//     paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : '0'
+//   },
+// });
