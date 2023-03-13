a0f0f275db1910fa15bdec1ac4998df6cd309f28
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-13 14:55:55 -0500

    notes on additional flex properties

--- App.js
+++ App.js
@@ -9,12 +9,16 @@ export default function App() {
   //one difference is flex boxes stack vertically by default in react native vs css where they stack horizontally.
   //to view all flexDirection options, simply delete and press ctrl + space to view
   //alignItems: 'stretch' is the default secondary axis'
+
+  //when items overflow on the main axis, one or more items will get shrunk to fit new item. flexWrap: 'noWrap' is default
+  //to fix this use flexWrap: "wrap"
   return (
       <View style={{
         backgroundColor: '#fff',
         flexDirection: 'row', // primary axis is swapped
         justifyContent: "center", //main
-        alignItems: 'flex-end', //secondary axis
+        alignItems: 'flex-end', //secondary axis (aligns items within each line)
+        alignContent: "center", //only works if you have wrapping
         flex: 1
       }}>
       <View style={{
