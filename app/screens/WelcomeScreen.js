96783063275e202999a1399e81df8e5b4608e57b
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-22 23:49:30 -0500

    adds two stylizes buttons to WelcomeScreen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet} from 'react-native'
+import { ImageBackground, StyleSheet, View} from 'react-native'
 import React from 'react'
 
 export default function WelcomeScreen() {
@@ -6,13 +6,22 @@ export default function WelcomeScreen() {
     <ImageBackground 
         style={styles.background}
         source={require("../assets/background.jpg")}
-    />
+    >
+        <View style={[styles.button, {backgroundColor: '#fc5c65'}]}></View>
+        <View style={[styles.button, {backgroundColor: '#4ecdc4'}]}></View>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
     background: {
         //image background will take the entire screen
-        flex: 1
+        flex: 1,
+        //by default the main axis is column
+        justifyContent: 'flex-end'
+    },
+    button: {
+        width: '100%',
+        height: 70,
     }
 })
