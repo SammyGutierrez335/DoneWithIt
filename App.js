e42bc96e21381a9e0e73c14b7f9fe49dde8e6689
Sam Gutierrez <samgutierrez335@gmail.com>
2023-03-22 23:48:57 -0500

    moves my code out of App.js and imports WelcomeScreen component

--- App.js
+++ App.js
@@ -1,68 +1,8 @@
-import { 
-    SafeAreaView, StyleSheet, View, TouchableNativeFeedback, Text, Image
-  } from 'react-native';
+import React from 'react'
+import WelcomeScreen from './app/screens/WelcomeScreen';
   
   export default function App() {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={{
-                    backgroundColor: 'grey',
-                    // alignSelf: 'flex-start'
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                    //flexShrink is not used as often but it informs views to shrink if there is an overflow
-                    }}>
-
-                <Image 
-                fadeDuration={1000}
-                resizeMode={'cover'}
-                source={{
-                    width: 460,
-                    height: 600,
-                    uri: "https://www.milgard.com/sites/default/files/styles/side_image/public/product-category/grid-image/content_block_700x500_tuscany01.jpg?itok=GHVWX1Ce"
-                }}
-                />
-                <View style={{position: 'absolute', top: 10, alignItems: 'center',}}>
-                    <Image 
-                    fadeDuration={1000}
-                    resizeMode={'contain'}
-                    source={require('./assets/d-letter-logo.png')}
-                    style={{width: 160, height: 160}}
-                    />
-                    <Text style={{ color: "red", fontSize: "16"}}>Done With It</Text>
-                </View>
-                    </View>
-            <View style={{
-                    backgroundColor: 'white',
-                    // alignSelf: 'flex-start'
-                    flexBasis: 100, //flex basis can map the width or height depending on which is the primary axis
-                    flexGrow: 1, //'flex' is shorthand for flexGrow (positive integers) and flexShrink (negative)
-                    //flexShrink is not used as often but it informs views to shrink if there is an overflow
-                    }}></View>
-            <TouchableNativeFeedback onPress={() => console.log("You clicked the Login Button")}>
-             <View style={{
-                    backgroundColor: 'salmon',
-                    // alignSelf: 'flex-start'
-                    flexBasis: 60, //flex basis can map the width or height depending on which is the primary axis
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                    }}><Text style={{color: 'white'}}>Login</Text></View>
-            </TouchableNativeFeedback>
-            <TouchableNativeFeedback onPress={() => console.log("You clicked the Register Button")}>
-             <View style={{
-                    backgroundColor: 'aqua',
-                    flexBasis: 60, //flex basis can map the width or height depending on which is the primary axis
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                    }}><Text style={{color: 'white'}}>Register</Text></View>
-            </TouchableNativeFeedback>
-        </SafeAreaView>
-    )
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-            flex: 1,
-        },
-  });
+        <WelcomeScreen />
+    )};
   
